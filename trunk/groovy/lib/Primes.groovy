7646906a97e4458538194f3409649f908dc6460e
tsukit <tsukit@cc57d79b-22db-b850-247a-0677f3b7e8bf>
2010-11-14 06:31:48 +0000

    finish problem 3 both languages

--- trunk/groovy/lib/Primes.groovy
+++ trunk/groovy/lib/Primes.groovy
@@ -7,9 +7,22 @@ class Primes extends InfiniteSequence {
 
    protected next(states) {
       def nextPrime = states.current
-      states.filters << states.current++
-      while(states.filters.any { states.current % it == 0 })
+      states.filters << states.current
+      def foundIt = false
+      while(!foundIt) {
+         // move to the next number and assume it's a prime
          ++states.current
+         foundIt = true
+
+         // apply the filter that's lower than half it
+         def limit = states.current / 2 as BigInteger
+         for (f in states.filters) {
+            if (f > limit) break
+            if (states.current % f == 0) {
+               foundIt = false
+            }
+         }
+      }
       return nextPrime
    }
 }
